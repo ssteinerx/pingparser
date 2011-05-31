3cf80df32fd90cf7106e849ebe0a09f15e4f9588
Gregg Gajic <gregggajic@gmail.com>
2011-05-31 16:26:12 -0500

    Add custom output formatting options during command-line use.

--- README.rst
+++ README.rst
@@ -5,31 +5,32 @@ pingparser parses the output of the system ping command.
 Usage
 ~~~~~
 You can pipe ping command output into pingparser.py on the command line, which
-outputs the parsed result to stdout as a JSON encoded string::
+outputs the parsed result to stdout in a customizable output format (see
+``./pingparser.py --help`` for details)::
 
-    $ ping -c 5 www.google.com | ./pingparser.py
-    {"received": "3", "jitter": "55.831", "avgping": "59.012", "minping": "17.304", "host": "www.l.google.com", "maxping": "137.927", "sent": "5"}
+  $ ping -c 5 www.google.com | ./pingparser.py +%h %s %r %m %M %a
+  www.l.google.com 5 5 14.711 26.378 19.621
 
 Or in Python you can call ``pingparser.parse(ping_output)``, which returns
 a dictionary containing the parsed fields::
 
-    >>> import pingparser
-    >>> pingparser.parse('''
-    ... PING www.l.google.com (74.125.225.84) 56(84) bytes of data.
-    ... 64 bytes from 74.125.225.84: icmp_req=1 ttl=55 time=13.9 ms
-    ... 64 bytes from 74.125.225.84: icmp_req=2 ttl=55 time=21.4 ms
-    ...
-    ... --- www.l.google.com ping statistics ---
-    ... 2 packets transmitted, 2 received, 0% packet loss, time 5072ms
-    ... rtt min/avg/max/mdev = 13.946/17.682/21.418/3.736 ms''')
-    {'received': '2', 'jitter': '3.736', 'avgping': '17.682', 'minping': '13.946', 'host': 'www.l.google.com', 'maxping': '21.418', 'sent': '2'}
+  >>> import pingparser
+  >>> pingparser.parse('''
+  ... PING www.l.google.com (74.125.225.84) 56(84) bytes of data.
+  ... 64 bytes from 74.125.225.84: icmp_req=1 ttl=55 time=13.9 ms
+  ... 64 bytes from 74.125.225.84: icmp_req=2 ttl=55 time=21.4 ms
+  ...
+  ... --- www.l.google.com ping statistics ---
+  ... 2 packets transmitted, 2 received, 0% packet loss, time 5072ms
+  ... rtt min/avg/max/mdev = 13.946/17.682/21.418/3.736 ms''')
+  {'received': '2', 'jitter': '3.736', 'avgping': '17.682', 'minping': '13.946', 'host': 'www.l.google.com', 'maxping': '21.418', 'sent': '2'}
 
 
 Installing
 ~~~~~~~~~~
 Installing from PyPI is easy::
 
-  $ pip install integer_encoding
+  $ pip install pingparser
 
 or you grab the source and run::
 
--- src/pingparser.py
+++ src/pingparser.py
@@ -4,10 +4,7 @@
 Parses the output of the system ping command.
 """
 
-try:
-    import simplejson as json
-except ImportError:
-    import json
+from optparse import OptionGroup,OptionParser
 
 import re
 import sys
@@ -47,11 +44,58 @@ def parse(ping_output):
 
 def main(argv=sys.argv):
     # detects whether input is piped in
+    ping_output = None
     if not sys.stdin.isatty():
         ping_output = sys.stdin.read()
 
-    result_dict = parse(ping_output)
-    sys.stdout.write(json.dumps(result_dict))
+    usage = 'Usage: %prog [OPTIONS] [+FORMAT]\n\n'\
+            'Parses output from the system ping command piped in via stdin.'
+    parser = OptionParser(usage=usage, version="%prog 0.1")
+
+    format_group = OptionGroup(parser,
+    """FORMAT controls the output. Interpreted sequences are:
+    \t%h    host name or IP address
+    \t%s    packets sent
+    \t%r    packets received
+    \t%m    minimum ping in milliseconds
+    \t%a    average ping in milliseconds
+    \t%M    maximum ping in milliseconds
+    \t%j    jitter in milliseconds
+
+    Default FORMAT is %h,%s,%r,%m,%a,%M,%j""")
+    parser.add_option_group(format_group)
+
+    (options, args) = parser.parse_args()
+
+    if ping_output is None:
+        parser.print_help()
+        sys.exit(1)
+
+    ping_result = parse(ping_output)
+
+    format_replacements = [('%h', 'host'),
+                           ('%s', 'sent'),
+                           ('%r', 'received'),
+                           ('%m', 'minping'),
+                           ('%a', 'avgping'),
+                           ('%M', 'maxping'),
+                           ('%j', 'jitter')]
+    format_replacements = [(fmt, ping_result[field]) for fmt, field in
+                           format_replacements]
+
+    if len(args) == 0:
+        output = ','.join(fmt for (fmt, rep) in format_replacements)
+    elif args[0].startswith('+'):
+        args[0] = args[0].lstrip('+')
+        output = ' '.join(args[0:])
+    else:
+        parser.print_help()
+
+    for (fmt, rep) in format_replacements:
+        output = output.replace(fmt, rep)
+
+    sys.stdout.write(output)
+
     sys.exit(0)
 
 if __name__ == "__main__":
